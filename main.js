a7277c11a137c6c04064b2de0f24a0520096fcab
bhoomish123 <105145006+bhoomish123@users.noreply.github.com>
2022-07-18 20:22:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+
+function setup(){
+ canvas = createCanvas(301, 301);
+ canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+  save('myFilterImage.png');
+}
